c5b956853b41147e0c38fb054af9486e51688d35
AxCooking <alexander.fross@gmx.net>
2026-05-03 11:51:44 +0200

    #2 Token Hardening: GH_PUBLIC_TOKEN to null (no longer used)

--- config.js
+++ config.js
@@ -7,7 +7,4 @@ const GH_BRANCH = 'main';
 const GH_API = 'https://api.github.com';
 // GH_PUBLIC_TOKEN: Ermöglicht öffentliches Lesen ohne Admin-Login.
 // Hinweis: Dieser Token ist bewusst nur-lesend (contents: read) auf dieses Repo beschränkt.
-const GH_PUBLIC_TOKEN = [
-  'gi','thub_','pat_11C','A2YSEQ08yRq75v1X1jx',
-  '_8TAdo74XckmQt1131hFPW','w4cOLQCGUCOlVhLHmhZhW7','USMCCUKF11pLnuIV'
-].join('');
+const GH_PUBLIC_TOKEN = null;
