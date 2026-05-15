0f35f091c83867243b1199b0301607aec07db4b8
AxCooking <alexander.fross@gmx.net>
2026-05-15 17:13:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,3 +8,5 @@ const GH_API = 'https://api.github.com';
 // GH_PUBLIC_TOKEN: Ermöglicht öffentliches Lesen ohne Admin-Login.
 // Hinweis: Dieser Token ist bewusst nur-lesend (contents: read) auf dieses Repo beschränkt.
 const GH_PUBLIC_TOKEN = null;
+// Auth-Feature aktiviert ab 2026-05-15 (PROD-DB + Worker v3.13 deployed)
+window.AUTH_ENABLED = true;
